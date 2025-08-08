a8a8543adf41b4bfbfe23418b697a793dddd277d
Xamuel Perez <xamuel.perez@gmail.com>
2025-08-08 12:18:22 -0500

    implementacion de archivos "ayuda", y eliminacion commit anterior

---
+++ ayuda.h
@@ -0,0 +1,13 @@
+#ifndef AYUDA_H
+#define AYUDA_H
+
+#include <string>
+using namespace std;
+
+// Muestra todos los comandos disponibles
+void mostrarAyudaGeneral();
+
+// Muestra la ayuda especifica de un comando dado
+void mostrarAyudaComando(const string& comando);
+
+#endif // AYUDA_H
