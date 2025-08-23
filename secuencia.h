b696bdcfa5eba3f807f9c69981d7a0f937b10be1
Rafael Held <joseheldgual@gmail.com>
2025-08-23 14:53:30 -0500

    Update secuencia.h

--- secuencia.h
+++ secuencia.h
@@ -1,14 +1,1 @@
-#ifndef SECUENCIA_H
-#define SECUENCIA_H
 
-#include <string>
-
-struct Secuencia {
-    std::string descripcion;
-    std::string bases;
-};
-
-#endif // SECUENCIA_H
-
-
-//es el contenedor donde se guarda en memoria cada secuencia que se lee de un archivo FASTA.
