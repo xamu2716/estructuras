6d933c2d486ecc34f181e6e6c0ff2097c56df7b8
Xamuel Perez <xamuel.perez@gmail.com>
2025-09-01 10:59:49 -0500

    Refactor main loop to improve input handling and integrate Genoma instance in line processing

--- main.cxx
+++ main.cxx
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <string>
+#include "genoma.h"
 #include "comandos.h"
+
 using namespace std;
 
 int main() {
+    Genoma genoma;             // instancia unica del estado en memoria
     string linea;
+
     cout << "$ ";
-    while (getline(cin, linea)) {
-        bool terminar = procesarLinea(linea);
-        if (terminar) break;
+    while (true) {
+        if (!getline(cin, linea)) {
+            break; // EOF o error de entrada
+        }
+        bool debeSalir = procesarLinea(linea, genoma);
+        if (debeSalir) {
+            break;
+        }
         cout << "$ ";
     }
     return 0;
